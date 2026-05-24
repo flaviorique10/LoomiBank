497c97362e704b3c5cfc0d9b6d7994899585047c
Flavio <flaflagi@gmail.com>
2026-05-24 14:29:21 -0300

    feat: implementa testes unitários para domínio e validações de clientes

---
+++ src/Customers/Customers.UnitTests/Domain/CustomerTests.cs
@@ -0,0 +1,24 @@
+﻿using Customers.Domain.Entities;
+using Customers.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Customers.UnitTests.Domain;
+
+public class CustomerTests
+{
+    [Fact]
+    public void UpdateProfilePicture_GivenValidUrl_ShouldUpdateProperty()
+    {
+        // Arrange
+        var banking = new BankingDetails("1234", "56789-0");
+        var customer = new Customer("João", "joao@email.com", "Rua Y", banking);
+        var newUrl = "https://azure.com/foto.jpg";
+
+        // Act
+        customer.UpdateProfilePicture(newUrl);
+
+        // Assert
+        customer.ProfilePictureUrl.Should().Be(newUrl);
+    }
+}
---
+++ src/Customers/Customers.UnitTests/Validators/CustomerUpdateRequestValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using Customers.Application.DTOs;
+using Customers.Application.Validators;
+using FluentAssertions;
+using Xunit;
+
+namespace Customers.UnitTests.Validators;
+
+public class CustomerUpdateRequestValidatorTests
+{
+    private readonly CustomerUpdateRequestValidator _validator;
+
+    public CustomerUpdateRequestValidatorTests()
+    {
+        // Arrange: Prepara o validador para todos os testes
+        _validator = new CustomerUpdateRequestValidator();
+    }
+
+    [Fact]
+    public void Validate_GivenValidRequest_ShouldNotHaveErrors()
+    {
+        // Arrange (Preparação)
+        var request = new CustomerUpdateRequestDto("João da Silva", "joao@email.com", "Rua Principal, 123", null);
+
+        // Act (Ação)
+        var result = _validator.Validate(request);
+
+        // Assert (Verificação)
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Validate_GivenInvalidEmail_ShouldHaveValidationError()
+    {
+        // Arrange
+        var request = new CustomerUpdateRequestDto("João da Silva", "email-invalido-sem-arroba", "Rua Principal, 123", null);
+
+        // Act
+        var result = _validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "Email");
+    }
+
+    [Fact]
+    public void Validate_GivenShortName_ShouldHaveValidationError()
+    {
+        // Arrange
+        var request = new CustomerUpdateRequestDto("Jo", "joao@email.com", "Rua Principal", null);
+
+        // Act
+        var result = _validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "Name");
+    }
+}
